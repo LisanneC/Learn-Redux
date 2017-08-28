d8ce0aa8fc3bd1e9b7d993d8f7d1254df44c6286
lisanne <lisanne.chin@gmail.com>
2017-08-28 17:02:42 +0200

    create components main

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,9 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+import css from './styles/style.styl'; //import css
+
+//import components
+import Main from './components/Main';
+
+render(<Main/>, document.getElementById('root'));
