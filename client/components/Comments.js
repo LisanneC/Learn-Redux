d38865fe466dcd3577c4b93f65d063e09387af02
lisanne <lisanne.chin@gmail.com>
2017-08-30 12:35:23 +0200

    add and delete comments

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -7,7 +7,7 @@ const Comments = React.createClass({
         <p>
           <strong>{comment.user}</strong>
           {comment.text}
-          <button className="remove-comment">&times;</button>
+          <button className="remove-comment" onClick={this.props.removeComment.bind(null, this.props.params.postId, i)}>&times;</button>
         </p>
       </div>
     )
@@ -18,6 +18,8 @@ const Comments = React.createClass({
     const author = this.refs.author.value;
     const comment = this.refs.comment.value;
     this.props.addComment(postId, author, comment);
+    // auto clear the form after input
+    this.refs.commentForm.reset();
   },
 
   render() {
